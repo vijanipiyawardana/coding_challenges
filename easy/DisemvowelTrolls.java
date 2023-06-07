d7f3a220750d5f957619b314cfe2420ee1f3c589
vijanipiyawardana <vijanipiyawardana@gmail.com>
2023-06-07 18:33:37 +0200

    DisemvowelTrolls: solved

---
+++ easy/DisemvowelTrolls.java
@@ -0,0 +1,20 @@
+/*
+* Challenge 5 - Disemvowel Trolls
+Problem: Trolls are attacking your comment section! A common way to deal with this situation is to remove 
+all of the vowels from the trolls' comments, neutralizing the threat.
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+Note: for this kata y isn't considered a vowel.
+*/ 
+
+class DisemvowelTrolls {
+
+    private static String removeVowels(String text) {
+        text = text.replaceAll("[aeiouAEIOU]", "");
+        return text;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(removeVowels("This website is for losers LOL!"));
+    }
+}
